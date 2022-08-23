90f732d0a1a2c1a56ab31261fa1a1dda3d22140e
enki-somer <enkisomer@gmail.com>
2022-08-23 19:24:45 +0300

    link cv mod

--- src/Components/Header/CTA.jsx
+++ src/Components/Header/CTA.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const CTA = () => {
     return (
         <div className='cta'>
-            <a href="https://drive.google.com/file/d/14Gkgy3vWHzYjb4TvbiktzPk5TKeV8udc/view?usp=sharing" target={"_blank"} className='btn' >Download CV</a>
+            <a href="https://drive.google.com/file/d/1oldA9mWWVmQ0YOMHUBUWjdTvGTsR4EnB/view?usp=sharing" target={"_blank"} className='btn' >Download CV</a>
             <a href="#contact" className='btn btn-primary' >Let's Talk</a>
 
         </div>
